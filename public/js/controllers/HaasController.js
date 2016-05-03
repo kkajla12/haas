b81de241c73350ec829c8daea74c4b0b92424f84
Robert Winders <rlwinders3@gmail.com>
2016-05-03 10:57:03 -0700

    ng-repeat message structure for chat area - fixed duplicate+ twilio messageAdded issue

--- public/js/controllers/HaasController.js
+++ public/js/controllers/HaasController.js
@@ -7,33 +7,41 @@ app.controller('HaasController', ['$scope', 'DataService', function($scope, Data
 
     var msg = new SpeechSynthesisUtterance();
     var voices = window.speechSynthesis.getVoices();
-    msg.voice = voices[16]; // Note: some voices don't support altering params
+    msg.voice = voices[1]; // Note: some voices don't support altering params
     msg.voiceURI = 'native';
     msg.volume = 1; // 0 to 1
     msg.rate = 0.9; // 0.1 to 10
     msg.pitch = 1.7; //0 to 2
     msg.lang = 'en-US';
 
+    var twilioChannel;
+
+    $scope.messages = [{'message': 'hello', 'class': 'message-user'}];
+
     $scope.init = function() {
         twilioToken = DataService.getTwilioToken();
         channelId = DataService.getChannelId();
-        console.log(twilioToken);
-        console.log(channelId);
         tc.accessManager = new Twilio.AccessManager(twilioToken);
         tc.messagingClient = new Twilio.IPMessaging.Client(tc.accessManager);
-    }
-
-    $scope.query = function () {
         tc.messagingClient.getChannelBySid(channelId).then(function(channel) {
             channel.join().then(function(joinedChannel) {
               console.log('Joined channel ' + joinedChannel.friendlyName);
-              joinedChannel.sendMessage($scope.request);
+              twilioChannel = joinedChannel
             });
             channel.on('messageAdded', function(message) {
               msg.text = message.body;
-              console.log(msg)
+              if(msg.text != $scope.request) {
+                $scope.messages.push({'message': msg.text, 'class': 'message-bot'});
+                $scope.$apply();
+              }
+              console.log($scope.messages);
             });
         });
+    }
+
+    $scope.query = function () {
+        $scope.messages.push({'message': $scope.request, 'class': 'message-user'});
+        twilioChannel.sendMessage($scope.request);
     }
 
     querySuccess = function(res) {
